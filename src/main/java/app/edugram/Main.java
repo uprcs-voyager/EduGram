d63f05abdf8d1e0b03c644a0623d2a386e508654
Zharif Aziz <clawsrage27@gmail.com>
2025-05-27 11:18:05 +0800

    Resolve conflicts

--- src/main/java/app/edugram/Main.java
+++ src/main/java/app/edugram/Main.java
@@ -10,7 +10,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
@@ -19,7 +18,6 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
